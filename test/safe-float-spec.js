f0fc915231ee81c559edfc2912a4f7a93a53b745
Vincent <homerdd@gmail.com>
2017-03-29 02:28:12 +0800

    Project init

---
+++ test/safe-float-spec.js
@@ -0,0 +1,37 @@
+const should = require('should')
+const SafeFloat = require('../lib/safe-float')
+
+describe('Plus', function () {
+  it('should always return string', function () {
+    SafeFloat.plus('0.111', '0.222').should.be.type('string')
+  })
+
+  it('should throw error unless pass string as params', function () {
+    should.throws(function () {
+      return SafeFloat.plus(1, 0)
+    }, Error, 'Only allow string arguments for precision reasons.')
+  })
+
+  it('should always be accurate in calculations.', function () {
+    SafeFloat.plus('0.111', '0.222').should.equal('0.333')
+    SafeFloat.plus('1.111', '0.00000001').should.equal('1.11100001')
+    SafeFloat.plus('0.000001', '.0').should.equal('0.000001')
+    SafeFloat.plus('1.0000999', '0.0000001').should.equal('1.0001')
+    SafeFloat.plus('1.0000999', '-0.0000001').should.equal('1.0000998')
+    SafeFloat.plus('1.9', '.5').should.equal('2.4')
+    SafeFloat.plus('1', '1.0').should.equal('2')
+    SafeFloat.plus('12', '88').should.equal('100')
+    SafeFloat.plus('12', '-88').should.equal('-76')
+  })
+})
+
+describe('Minus', function () {
+  it('should always be accurate in calculations.', function () {
+    SafeFloat.minus('0.111', '0.222').should.equal('-0.111')
+    SafeFloat.minus('0.111', '-0.222').should.equal('0.333')
+    SafeFloat.minus('1', '2').should.equal('-1')
+    SafeFloat.minus('-2.99', '0.99').should.equal('-3.98')
+    SafeFloat.minus('-2', '0.222').should.equal('-2.222')
+    SafeFloat.minus('-2', '-4.52').should.equal('2.52')
+  })
+})
